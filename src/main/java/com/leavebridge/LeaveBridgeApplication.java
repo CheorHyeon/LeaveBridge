bbdb577be84e6698563f18b18199b497ecced158
cheorhyeon <r4560798@naver.com>
2025-06-21 18:05:25 +0900

    setting : project init

---
+++ src/main/java/com/leavebridge/LeaveBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.leavebridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leavebridge/LeaveBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leavebridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
