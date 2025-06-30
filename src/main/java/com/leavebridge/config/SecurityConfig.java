35671d6f73d233a7b6e39c1df7ef097a04ddca3f
cheorhyeon <r4560798@naver.com>
2025-06-30 22:16:08 +0900

    refactor : 테스트를 위한 임시 권한 모두 허용 + @PreAuthorize 사용을 위한 어노테이션 추가

--- src/main/java/com/leavebridge/config/SecurityConfig.java
+++ src/main/java/com/leavebridge/config/SecurityConfig.java
@@ -3,15 +3,16 @@ package com.leavebridge.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)  // @PreAuthorize 사용하기 위해 필요
 public class SecurityConfig {
 
 	@Bean
@@ -28,7 +29,7 @@ public class SecurityConfig {
 				.requestMatchers(HttpMethod.GET, "/").permitAll() // 메인 페이지 누구나 가능
 				// 예외 처리 로직 없을 때 Tomcat이 포워딩 하는 경로 접근 허용하여 예외 메세지 잘 전달되도록 허용
 				.requestMatchers("/error").permitAll()
-				.anyRequest().authenticated() // 나머지는 인증된 사용자만 가능
+				.anyRequest().permitAll() // 나머지는 인증된 사용자만 가능
 			)
 
 			.logout(
