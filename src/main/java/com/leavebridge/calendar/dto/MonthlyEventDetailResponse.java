67d0c20157ed22d84a661c83f2af700a76145aaf
cheorhyeon <r4560798@naver.com>
2025-07-16 23:32:07 +0900

    refactor : 일정 상세에서 일자, 시간 따로 내려주도록 수정

--- src/main/java/com/leavebridge/calendar/dto/MonthlyEventDetailResponse.java
+++ src/main/java/com/leavebridge/calendar/dto/MonthlyEventDetailResponse.java
@@ -1,6 +1,7 @@
 package com.leavebridge.calendar.dto;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import com.leavebridge.calendar.entity.LeaveAndHoliday;
 import com.leavebridge.calendar.enums.LeaveType;
@@ -14,10 +15,14 @@ public record MonthlyEventDetailResponse(
 	Long id,
 	@Schema(description = "일정 제목", example = "박철현 연차")
 	String title,
-	@Schema(description = "시작 시간", example = "2025-07-01T14:00:00")
-	LocalDateTime startDate,
-	@Schema(description = "종료 시간", example = "2025-07-01T14:00:00")
-	LocalDateTime endDate,
+	@Schema(description = "시작 일자", example = "2025-07-01")
+	LocalDate startDate,
+	@Schema(description = "시작 시간", example = "08:00:00")
+	LocalTime startTime,
+	@Schema(description = "종료 일자", example = "2025-07-01")
+	LocalDate endDate,
+	@Schema(description = "종료 시간", example = "14:00:00")
+	LocalTime endTime,
 	@Schema(description = "하루 종일 이벤트 여부", example = "true or false")
 	Boolean isAllDay,
 	@Schema(description = "연차 종류", example = "HOLIDAY(공휴일), FULL_DAY_LEAVE(1일 연차), HALF_DAY_LEAVE(반차),  OUT_GOING(외출), SUMMER_VACATION(여름 휴가), OTHER_PERSON(비회원 연차)")
@@ -33,8 +38,10 @@ public record MonthlyEventDetailResponse(
 		return MonthlyEventDetailResponse.builder()
 			.id(leaveAndHoliday.getId())
 			.title(leaveAndHoliday.getTitle())
-			.startDate(LocalDateTime.of(leaveAndHoliday.getStartDate(), leaveAndHoliday.getStarTime()))
-			.endDate(LocalDateTime.of(leaveAndHoliday.getEndDate(), leaveAndHoliday.getEndTime()))
+			.startDate(leaveAndHoliday.getStartDate())
+			.startTime(leaveAndHoliday.getStarTime())
+			.endDate(leaveAndHoliday.getEndDate())
+			.endTime(leaveAndHoliday.getEndTime())
 			.isAllDay(leaveAndHoliday.getIsAllDay())
 			.leaveType(leaveAndHoliday.getLeaveType())
 			.description(leaveAndHoliday.getDescription())
