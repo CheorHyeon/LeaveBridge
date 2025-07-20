d98b1ce668439148f0222d5da34ed11ed074e807
cheorhyeon <r4560798@naver.com>
2025-07-20 10:30:38 +0900

    refactor : restful API 변경 + 연차 사용 현황 초안 페이지

--- src/main/java/com/leavebridge/member/controller/MemberController.java
+++ src/main/java/com/leavebridge/member/controller/MemberController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api/v1/member")
+@RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 @Slf4j
 public class MemberController {
@@ -32,7 +32,7 @@ public class MemberController {
 		return ResponseEntity.ok(memberService.getMemberUsedLeaves());
 	}
 
-	@PostMapping("/change-passwords")
+	@PatchMapping("/me/password")
 	public ResponseEntity<Void> changePassword(
 		@RequestBody @Valid RequestChangePasswordRequest requestChangePasswordRequest,
 		@AuthenticationPrincipal CustomMemberDetails customMemberDetails) {
